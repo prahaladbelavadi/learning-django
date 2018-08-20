4380e756bf14a7570d2a2fa07314d81d0f4465da
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-08-21 04:36:26 +0530

    added view

--- myPoll/polls/urls.py
+++ myPoll/polls/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
+    # example: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # example: /polls/5/results
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # example: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]

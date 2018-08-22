860700fa12b788ddeefbaa8bd36448987be1a0f1
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-08-23 02:34:38 +0530

    amend url.conf touse generic views

--- myPoll/polls/urls.py
+++ myPoll/polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
+    url(r'^$',views.IndexView.asView(), name='index'),
     # example: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.DetailView.asView(), name='detail'),
     # example: /polls/5/results
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.ResultsView.asView(), name='results'),
     # example: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]

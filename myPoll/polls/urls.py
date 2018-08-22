c8156479a0d230af0388ffb666da99fd7a9f9657
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-08-23 02:44:24 +0530

    change to generic view; next is testing but choices are not displayed; need to evaluatewhy and compare projects

--- myPoll/polls/urls.py
+++ myPoll/polls/urls.py
@@ -4,11 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$',views.IndexView.asView(), name='index'),
-    # example: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.DetailView.asView(), name='detail'),
-    # example: /polls/5/results
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.ResultsView.asView(), name='results'),
-    # example: /polls/5/vote/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- myPoll/polls/views.py
+++ myPoll/polls/views.py
@@ -3,6 +3,8 @@ from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
+
 from .models import Choice, Question
 
 
@@ -10,6 +12,24 @@ from .models import Choice, Question
 
 # def index(request):
 #     return HttpResponse("Hello World, We're at the polls index")
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        retun last 5 published questions.
+        """
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
